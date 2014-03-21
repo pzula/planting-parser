673ac6bf07c62a51bd644ab8f840f5017dbb3c4e
Persa Zula <persa@persazula.com>
2014-03-21 18:08:16 -0400

    Adds google_calendar dependency

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "http://rubygems.org"
 ruby "2.1.1"
 
 gem 'rake'
+gem 'google_calendar'
 
 group :test do
   gem 'minitest'
--- lib/planting-parser.rb
+++ lib/planting-parser.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'time'
+require 'google_calendar'
 require_relative 'planting-parser/parser'
+require_relative 'planting-parser/calendar_event'
 
 module PlantingParser
 end
