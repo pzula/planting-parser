4047a770a366bf353fa6d89f97d5e7ae161bac4c
Persa Zula <persa@persazula.com>
2014-03-23 21:01:13 -0400

    Adds google_calendar gem integration for posting events

--- lib/planting-parser/calendar_event.rb
+++ lib/planting-parser/calendar_event.rb
@@ -1,7 +1,27 @@
 module PlantingParser
   class CalendarEvent
     def initialize(title, date)
-      return "Successfully posted to Google"
+      e = Event.new
+      e.post(title, date, Time.now)
+    end
+  end
+
+  class Event
+    attr_reader :cal
+
+    def initialize
+      @cal = Google::Calendar.new(:username => ENV['GCAL_USERNAME'],
+                           :password => ENV['GCAL_PASSWORD'],
+                           :app_name => ENV['GCAL_APPNAME'])
+    end
+
+    def post(title, start_time, end_time)
+      event = cal.create_event do |e|
+        e.title = title
+        e.start_time = start_time
+        e.end_time = end_time
+      end
+      puts event
     end
   end
 end
