bcb22b2827b49e6fdcb8b7f39d48099250a40e03
Persa Zula <persa@persazula.com>
2014-03-29 11:51:14 -0400

    Adds a spec for the Event class and updates the post method

--- lib/planting-parser/calendar_event.rb
+++ lib/planting-parser/calendar_event.rb
@@ -1,26 +1,19 @@
 module PlantingParser
-  class CalendarEvent
-    def initialize(title, date)
-      e = Event.new
-      e.post(title, date, Time.now)
-    end
-  end
-
   class Event
     attr_reader :cal
 
     def initialize
       @cal = Google::Calendar.new(:username => ENV['GCAL_USERNAME'],
-                           :password => ENV['GCAL_PASSWORD'],
-                           :app_name => ENV['GCAL_APPNAME'])
+                                  :password => ENV['GCAL_PASSWORD'],
+                                  :app_name => ENV['GCAL_APPNAME'])
     end
 
-    def post(title, start_date, end_date)
+    def post(title, dates)
       event = cal.create_event do |e|
         e.title = title
-        e.start_time = start_date
-        if end_date
-          e.end_time = end_date
+        e.start_time = dates[0]
+        if dates[1]
+          e.end_time = dates[1]
         end
       end
       puts event
--- test/calendar_event_test.rb
+++ test/calendar_event_test.rb
@@ -1,1 +1,28 @@
 require_relative './test_helper'
+
+describe PlantingParser::Event do
+  def setup
+    @parsed_row = {
+      :date =>'2/17-2/19',
+      :alt_dates => '3/31-4/2',
+      :crop =>  "Broccoli",
+      :location => nil,
+      :direct_seed => nil,
+      :start => "X",
+      :transplant => nil,
+      :_plants => "20",
+      :_starts => "20",
+      :variety => "Di Cicco"
+    }
+  end
+
+  it('creates a properly formatted calendar event') do
+    event = PlantingParser::Event.new
+    event.post("Start 20 Di Cicco Brocolli", PlantingParser::Parser.new.date_splitter(@parsed_row[:date]))
+  end
+
+  it('creates a proper event without second date') do
+    event = PlantingParser::Event.new
+    event.post("Start 20 Di Cicco Brocolli", PlantingParser::Parser.new.date_splitter('2/28'))
+  end
+end
